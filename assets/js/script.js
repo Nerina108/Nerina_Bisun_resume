92f59dbdfd9c58efa251990c088485db7a45d219
Nerina108 <60883815+Nerina108@users.noreply.github.com>
2020-02-21 04:18:12 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//Scroll effect with section is clicked//
+$('.navbar').on('click', 'a[href^="#"]', function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
